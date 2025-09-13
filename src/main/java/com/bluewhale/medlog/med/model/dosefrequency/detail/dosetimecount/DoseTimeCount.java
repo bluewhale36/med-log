addfad7b6c51014bdc5fb63f4cdefe24171197e5
bluewhale36 <kmsuhyn36@gmail.com>
2025-09-13 10:09:16 +0900

    약 등록 - 시간별 약 복용 개수 등록 기능 1단계 구현 진행 중
    - DoseTime 과 DoseCount 만 추가하여 생성, 수정, 조회 로직에서 가용하게 만드는 작업 진행 중

--- src/main/java/com/bluewhale/medlog/med/model/dosefrequency/detail/dosetimecount/DoseTimeCount.java
+++ src/main/java/com/bluewhale/medlog/med/model/dosefrequency/detail/dosetimecount/DoseTimeCount.java
@@ -1,18 +1,25 @@
 package com.bluewhale.medlog.med.model.dosefrequency.detail.dosetimecount;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalTime;
 
 @Getter
 @ToString
-@RequiredArgsConstructor
-@Builder
 public class DoseTimeCount {
 
     private final LocalTime doseTime;
-    private final byte count;
+    private final byte doseCount;
+
+    @JsonCreator
+    public DoseTimeCount(
+            @JsonProperty("doseTime") LocalTime doseTime,
+            @JsonProperty("doseCount") byte doseCount
+    ) {
+        this.doseTime = doseTime;
+        this.doseCount = doseCount;
+    }
 }
