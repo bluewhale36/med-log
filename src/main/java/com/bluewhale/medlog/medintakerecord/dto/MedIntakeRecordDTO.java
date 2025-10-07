6850e99c9d588c64cc8e82141db3f9cc0e8bc7bd
bluewhale36 <kmsuhyn36@gmail.com>
2025-10-07 11:37:22 +0900

    Redis 활용 Caching 로직 구현 진행 중
    - 약 복용 기록 화면에서 Redis Cache 사용 구현 진행 중
    - RecordDayViewDTO 생성 로직을 UseCase 에서 Service Layer 로 분리. 재사용성 증진 목적. 추후 리팩토링 예정.

--- src/main/java/com/bluewhale/medlog/medintakerecord/dto/MedIntakeRecordDTO.java
+++ src/main/java/com/bluewhale/medlog/medintakerecord/dto/MedIntakeRecordDTO.java
@@ -4,40 +4,23 @@ import com.bluewhale.medlog.med.domain.value.MedUuid;
 import com.bluewhale.medlog.medintakerecord.domain.entity.MedIntakeRecord;
 import com.bluewhale.medlog.medintakerecord.domain.value.MedIntakeRecordUuid;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
-@Getter
-@ToString
+// Jackson Library 와 호환을 위해 Record Class 사용.
 @Builder
-public class MedIntakeRecordDTO {
-
-    private final MedIntakeRecordUuid medIntakeRecordUuid;
-    private final MedUuid medUuid;
-    private final boolean isTaken;
-    private final LocalDateTime estimatedDoseTime;
-    private final LocalDateTime takenAt;
-
-    @JsonCreator
-    public MedIntakeRecordDTO(
-            @JsonProperty("medIntakeRecordUuid") MedIntakeRecordUuid medIntakeRecordUuid,
-            @JsonProperty("medUuid") MedUuid medUuid,
-            @JsonProperty("isTaken") boolean isTaken,
-            @JsonProperty("estimatedDoseTime") LocalDateTime estimatedDoseTime,
-            @JsonProperty("takenAt") LocalDateTime takenAt
-    ) {
-        this.medIntakeRecordUuid = medIntakeRecordUuid;
-        this.medUuid = medUuid;
-        this.isTaken = isTaken;
-        this.estimatedDoseTime = estimatedDoseTime;
-        this.takenAt = takenAt;
-    }
-
+public record MedIntakeRecordDTO(
+        MedIntakeRecordUuid medIntakeRecordUuid,
+        MedUuid medUuid,
+        @JsonGetter("isTaken") boolean isTaken,
+        LocalDateTime estimatedDoseTime,
+        LocalDateTime takenAt
+) {
     public static MedIntakeRecordDTO from(MedIntakeRecord entity) {
         return MedIntakeRecordDTO.builder()
                 .medIntakeRecordUuid(entity.getMedIntakeRecordUuid())
